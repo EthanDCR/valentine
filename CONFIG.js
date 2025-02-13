ecf2bd37a058d501ac7df5dbcefb08930ddc9594
EthanDCR <clinteth.dev@gmail.com>
2025-02-12 20:21:34 -0600

    finisehd

--- CONFIG.js
+++ CONFIG.js
@@ -1,10 +1,10 @@
 const CONFIGDATA = {
-  titleHeader: "Chào cậu, tớ có điều muốn nói. </br> Làm người yêu tớ nhé!",
+  titleHeader: "Hey, I have something to ask you.Will you be my Valentine?",
   descriptionHeader:
-    "Hãy trả lời một cách thật lòng nhé. Nếu cậu im lặng và rời đi thì tức là cậu đã ĐỒNG Ý.",
-  buttonYes: "Tớ đồng ý",
-  buttonNo: "Không đồng ý",
-  titleModar: "Cảm ơn cậu.",
+    "If you stay silent and leave, that means you’ve already said YES!",
+  buttonYes: "Yes, of course!",
+  buttonNo: "No way!",
+  titleModar: "Thank you!",
   descriptionModar:
-    "Cảm ơn cậu đã cho tớ một cơ hội để tớ có thể ở bên cạnh, che chở và bảo vệ cậu. </br> Tớ biết tớ không được đẹp trai, cũng chẳng lắm tài nhưng tớ có một trái tim chân thành. Cậu chính là cả thế giới của tớ. </br> Tối nay tớ sẽ đến đón cậu. Cậu mặc thật đẹp vào nhé! Sẽ có một bất ngờ dành cho cậu.</br> Love you <3",
+    "<Thank you for being mine.<br>I love you more than coffee.<br>I met you. I liked you. I love you. I’m keeping you.<br>You may hold my hand for a while, but you hold my heart forever.<br>Whenever I'm with you, wherever we are, I'm home.</br><br></br> Love you <3",
 };
